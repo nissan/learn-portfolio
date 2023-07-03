93838a00cc22cfe6e282dde51e8855336fed5be6
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2023-07-04 06:30:19 +1000

    fix stylings, add a zustand page

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,21 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+          See the portfolio of things I am learning about&nbsp;
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
+          <span
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
           >
-            By{' '}
+            I built this using templates for <Image
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              src="/next.svg"
+              alt="Next.js Logo"
+              width={100}
+              height={24}
+              priority
+            />
+            and configured automatic deployments of this code to{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -25,84 +29,42 @@ export default function Home() {
               height={24}
               priority
             />
-          </a>
+          </span>
         </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        Highlight Reel
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Link href="/reactflow">
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            ReactFlow{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Some sample work in ReactFlow          </p>
-        </Link>
+        <div className="my-4" >
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <Link href="/reactflow" >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Learning ReactFlow{' '}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+          </Link>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Page with work done following the <Link className="text-red-300" href="https://reactflow.dev/docs/quickstart/">docs</Link> for <Link className="text-red-300" href="https://reactflow.dev/">ReactFlow</Link>
+          </p>
+        </div>
+        <div className="my-4" >
+          <h2 className={`mb-3 text-2xl font-semibold `}>
+            <Link href="/zustand">
+              Learning Zustend{' '}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </Link>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            Page with work done learning <Link className="text-red-300" href="https://github.com/pmndrs/zustand#readme">Zustand</Link> following <Link className="text-red-300" href="https://www.youtube.com/watch?v=fZPgBnL2x-Q">this video tutorial</Link>          </p>
+        </div>
       </div>
-    </main>
+    </main >
   )
 }
--- app/reactflow/page.tsx
+++ app/reactflow/page.tsx
@@ -21,16 +21,16 @@ const initialNodes = [
     {
         id: "1",
         position: {
-            x: 0,
-            y: 0
+            x: 150,
+            y: 50
         },
         data: { label: "Node 1" }
     },
     {
         id: "2",
         position: {
-            x: 0,
-            y: 100
+            x: 50,
+            y: 300
         },
         data: { label: "Node 2" }
     },
@@ -74,38 +74,49 @@ function ReactFlowPage() {
         { name: 'search', icon: <MdSearch size={24} /> },
     ];
     return (
-        <ReactFlowProvider>
-            <h2>Sample is here.</h2>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                    Sample portfolio of things I built learning about Reactflow&nbsp;
+                </p>
 
-            <div style={{ width: '100vw', height: '100vh' }}>
-                <ReactFlow
-                    nodes={nodes}
-                    edges={edges}
-                    onNodesChange={onNodesChange}
-                    onEdgesChange={onEdgesChange}
-                    onConnect={onConnect}
-                    minZoom={0.2}
-                    maxZoom={4}
-                    fitView={true}
-                >
-                    {nodes.map((node) => (
-                        <>
-                            <NodeToolbar
-                                nodeId={node.id}
+
+                <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+                    <ReactFlowProvider>
+                        <div style={{ width: '100vw', height: '100vh' }}>
+                            <ReactFlow
+                                nodes={nodes}
+                                edges={edges}
+                                onNodesChange={onNodesChange}
+                                onEdgesChange={onEdgesChange}
+                                onConnect={onConnect}
+                                minZoom={0.2}
+                                maxZoom={4}
+                                fitView={false}
                             >
-                                <ColorWheel
-                                    operations={operations}
-                                />
-                            </NodeToolbar>
-                            <NodeResizer nodeId={node.id} isVisible={true} />
-                        </>
-                    ))}
-                    <Controls />
-                    <MiniMap />
-                    <Background />
-                </ReactFlow>
+                                {nodes.map((node) => (
+                                    <>
+                                        <NodeToolbar
+                                            nodeId={node.id}
+                                        >
+                                            <ColorWheel
+                                                operations={operations}
+                                            />
+                                        </NodeToolbar>
+                                        <NodeResizer nodeId={node.id} isVisible={true} />
+                                    </>
+                                ))}
+                                <Controls />
+                                <MiniMap />
+                                <Background />
+                            </ReactFlow>
+                        </div>
+                    </ReactFlowProvider>
+                </div>
+
             </div>
-        </ReactFlowProvider>
+        </main>
+
     )
 }
 
